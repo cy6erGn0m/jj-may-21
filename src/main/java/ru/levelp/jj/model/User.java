5f35f1787726a913944d6ecdfbf817f8b748065b
Sergey Mashkov <cy6ergn0m@gmail.com>
2021-06-07 19:36:22 +0300

    Add entity getters/setters

--- src/main/java/ru/levelp/jj/model/User.java
+++ src/main/java/ru/levelp/jj/model/User.java
@@ -26,4 +26,36 @@ public class User {
 
     public User() {
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
 }
