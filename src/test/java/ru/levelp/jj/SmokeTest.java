a75849175b7616660ed2dcf163dff86aba8e2ea5
Sergey Mashkov <cy6ergn0m@gmail.com>
2021-06-07 19:36:40 +0300

    Add example query

--- src/test/java/ru/levelp/jj/SmokeTest.java
+++ src/test/java/ru/levelp/jj/SmokeTest.java
@@ -1,11 +1,14 @@
 package ru.levelp.jj;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.levelp.jj.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import static org.junit.Assert.assertNotNull;
 
 public class SmokeTest {
     @Test
@@ -17,6 +20,14 @@ public class SmokeTest {
         entityManager.getTransaction().begin();
         entityManager.persist(user);
         entityManager.getTransaction().commit();
+
+        User foundUser = entityManager.find(User.class, user.getId());
+        assertNotNull(foundUser);
+
+        User foundByQuery = entityManager.createQuery("select u from User u where u.id = :id", User.class)
+                .setParameter("id", user.getId())
+                .getSingleResult();
+        assertNotNull(foundByQuery);
 
         entityManager.close();
         factory.close();
