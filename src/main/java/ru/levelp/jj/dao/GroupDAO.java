c751e66adb3afd52637ed34e3590ee46e71851a0
Sergey Mashkov <cy6ergn0m@gmail.com>
2021-06-28 21:34:39 +0300

    Add Rest Repositories

--- src/main/java/ru/levelp/jj/dao/GroupDAO.java
+++ src/main/java/ru/levelp/jj/dao/GroupDAO.java
@@ -3,6 +3,7 @@ package ru.levelp.jj.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.levelp.jj.model.Group;
@@ -10,6 +11,10 @@ import ru.levelp.jj.model.Group;
 import java.util.List;
 
 @Repository
+@RepositoryRestResource(
+        collectionResourceRel = "groups",
+        itemResourceRel = "group"
+)
 public interface GroupDAO extends JpaRepository<Group, Integer> {
     @Transactional
     default Group create(String groupName) {
