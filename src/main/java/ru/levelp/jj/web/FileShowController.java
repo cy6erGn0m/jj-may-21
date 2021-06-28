3021121e27748e67b93d875491972a1b5603f02b
Sergey Mashkov <cy6ergn0m@gmail.com>
2021-06-28 22:20:41 +0300

    Add image file streaming

--- src/main/java/ru/levelp/jj/web/FileShowController.java
+++ src/main/java/ru/levelp/jj/web/FileShowController.java
@@ -2,16 +2,20 @@ package ru.levelp.jj.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import ru.levelp.jj.dao.FileUploadItemRepository;
 import ru.levelp.jj.model.FileUploadItem;
 
-import java.io.InputStream;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Optional;
 
 @RestController
 public class FileShowController {
@@ -37,18 +41,30 @@ public class FileShowController {
 
     @GetMapping("/file/show/{id}")
     @ResponseBody
-    public ResponseEntity<byte[]> showFile(@PathVariable int id) throws SQLException {
-        FileUploadItem file = items.findById(id).orElseThrow();
-        byte[] content = file.getFileContent().getBytes(0, (int)file.getSize());
+    public ResponseEntity<StreamingResponseBody> showFile(@PathVariable int id) {
+        Optional<FileUploadItem> item = items.findById(id);
+        if (item.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        FileUploadItem file = item.get();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "image/png");
+        headers.setContentType(MediaType.IMAGE_PNG);
+        headers.setContentLength(file.getSize());
 
-        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(content,
+        StreamingResponseBody body = outputStream -> {
+            try {
+                StreamUtils.copy(file.getFileContent().getBinaryStream(), outputStream);
+            } catch (SQLException cause) {
+                throw new IOException(cause);
+            }
+        };
+
+        return new ResponseEntity<>(
+                body,
                 headers,
                 200
         );
-
-        return response;
     }
 }
